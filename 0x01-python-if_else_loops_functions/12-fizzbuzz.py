617e254a97926a67c880684e3c02996150f407b7
starkjoy <residualcode6@gmail.com>
2023-03-08 08:20:18 -0800

    python flow controls

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for fizzbuzz in range(1, 100):
-        if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
-            print("fizzbuzz")
-            continue
-        elif fizzbuzz % 3 == 0:
-            print("fizz")
-            continue
-        elif fizzbuzz % 5 == 0:
-            print("buzz")
-            continue
-        print(fizzbuzz)
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("Fizzbuzz", end='')
+        elif i % 3 == 0:
+            print("Fizz", end='')
+        elif i % 5 == 0:
+            print("Buzz", end='')
+        else:
+            print(str(i) + ' ', end='')
 
