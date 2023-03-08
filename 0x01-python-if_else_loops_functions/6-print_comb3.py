04fbf86a8dfa4bf67fa9aa31f7c94ce9c0491c5c
starkjoy <residualcode6@gmail.com>
2023-03-08 08:57:53 -0800

    python flow controls

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i, 10):
-        print(f"{i}{j}", end=", ")
-    print("89")
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
