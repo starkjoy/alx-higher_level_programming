bf93abb26a51e34df7cbf50572452b8573f152b3
starkjoy <residualcode6@gmail.com>
2023-03-08 09:01:26 -0800

    python flow controls

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,5 @@ def uppercase(str):
     for c in s:
         if ord(c) >= 97 and ord(c) <= 122:
             upper_c = chr(ord(c) - 32)
-            print("{0}".format(upper_c), end='')
-        else:
-            print("{0}".format(c), end='')
-    print()
+            print("{0}".format(uppercase), end="")
+    print("")
