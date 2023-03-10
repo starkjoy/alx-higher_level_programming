c9bcdf07a6fcb4db12e7a439a54a0cbd5e15afa8
starkjoy <residualcode6@gmail.com>
2023-03-10 07:51:11 -0800

    Python Modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import calculator_1.py
-a = 10
-b = 5
 if __name == "__main__":
+import calculator_1.py
+    a = 10
+    b = 5
     print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
     print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
     print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
