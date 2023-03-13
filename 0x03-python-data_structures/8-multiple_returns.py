a3ce6b3f6d26014e2d2be585333a8b99b372dd61
starkjoy <residualcode6@gmail.com>
2023-03-13 10:47:15 -0700

    Python Data Structures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
+    if sentence == "":
         return (0, None)
-    return  (len(sentence), sentence[0]i)
+    return  (len(sentence), sentence[0])
