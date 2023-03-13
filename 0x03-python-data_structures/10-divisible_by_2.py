1d0eeee07dcdc93710d117219e93f7da9786e04b
starkjoy <residualcode6@gmail.com>
2023-03-13 11:08:05 -0700

    Python Data Structures

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def divisible_by_2(my_list=[]):
-        return ([num % 2 == 0 for num in my_list])
+def divisible_by_2(my_list=[]):
+    return ([num % 2 == 0 for num in my_list])
