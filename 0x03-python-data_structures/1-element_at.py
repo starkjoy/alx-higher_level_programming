027a6037a123b181bdff04988886e57570fd1baf
starkjoy <residualcode6@gmail.com>
2023-03-13 09:42:05 -0700

    Python Data Structures

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def element_at(my_list, idx):
-        if idx < 0:
-            return ("None")
-        elif idx > len(my_list) - 1:
-            return ("None")
-        else:
-            return (my_list[idx])
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx > (len(my_list) - 1):
+        return None
+    else:
+        return (my_list[idx])
