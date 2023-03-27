54671bcbb447282b760109f8bdccf8bdd5624285
starkjoy <residualcode6@gmail.com>
2023-03-27 07:48:57 -0700

    Pyhton Exceptions

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            x = my_list_1[i]
+            y = my_list_2[i]
+            if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+                raise TypeError("wrong type")
+            if y == 0:
+                raise ZeroDivisionError("division by 0")
+            result.append(x / y)
+        except IndexError:
+            print("out of range")
+            result.append(0)
+        except TypeError:
+            print("wrong type")
+            result.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+        finally:
+            pass
+    return result
