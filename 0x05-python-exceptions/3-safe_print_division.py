4121c41850b48012c61a08a97fda6cef7e8b7b86
starkjoy <residualcode6@gmail.com>
2023-03-27 08:37:25 -0700

    Pyhton Exceptions

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,8 @@
 def safe_print_division(a, b):
     try:
         c = a / b
-    except ZeroDivisionError:
-        return None
+    except (TypeError, ZeroDivisionError):
+        c = None
     finally:
         print("Inside result: {}".format(c))
+    return (c)
