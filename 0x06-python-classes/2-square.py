d59aaf624974d11d3973497de284b30f8be501d0
starkjoy <residualcode6@gmail.com>
2023-03-28 10:25:08 -0700

    Python classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,6 +4,10 @@
 
 class Square:
     def __init__(self, size=0):
+        ''' Makes a square
+
+        Args: size: size of square
+        '''
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
