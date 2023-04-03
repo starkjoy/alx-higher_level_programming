de107852c7cf44a240a3dc478f868f48281c1c5c
Joy <117626913+starkjoy@users.noreply.github.com>
2023-04-03 17:57:25 +0000

    More Python Classes

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -53,6 +53,14 @@ class Rectangle:
         else:
             return 2 * (self.width + self.height)
 
+    @classmethod
+    def square(cls, size=0):
+        """ returns a new rectangle instance width == height == size 
+        Args:
+            size: size of square
+        """
+        return (cls(size, size))
+
     def __str__(self):
         """ string representation of rectangle """
         if self.width == 0 or self.height == 0:
@@ -88,12 +96,4 @@ class Rectangle:
         elif area(rect_1) < area(rect_2):
             return rect_2
 
-    @classmethod
-    def square(cls, size=0):
-        """ returns a new rectangle instance width == height == size 
-
-        Args:
-            size: size of square
-
-        """
-        return (cls(size, size))
+    
