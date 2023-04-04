a41b39dad35c7f24aaf60512ba39899ca84d2bbc
starkjoy <residualcode6@gmail.com>
2023-04-04 05:54:33 -0700

    Python objects

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.n = getattr(magic_string, 'n' ,0) + 1
-    return "Holberton, " * (magic_string.n - 1) + "Holberton"
+    magic_string.n = getattr(magic_string, 'counter' ,0) + 1
+    return ', '.join(['BestSchool'] * magic_string.counter)
