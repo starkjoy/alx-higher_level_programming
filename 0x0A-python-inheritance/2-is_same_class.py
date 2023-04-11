287c609e8f1b92e7a550d74fa724053e2af543fc
starkjoy <residualcode6@gmail.com>
2023-04-11 09:44:31 -0700

    Python Inheritance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,10 +3,10 @@
 
 
 def is_same_class(obj, a_class):
-    """ 
+    """
     checks if it is an instance of the class
     obj: object
     a_class: class
     return: true or false for case of object being an instance of class
     """
-    return type(obj) ==  a_class
+    return type(obj) == a_class
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Returns True if object is an instance of a class that inherited from the specified class"""
+""" Returns True if object is an instance of a class """
 
 
 def inherits_from(obj, a_class):
-    """ 
+    """
     checks if it is an instance of an inherited class
     obj: object
     a_class: class
-    return: true or false for case where object is instance of class inheriting from specified class
+    return: true or false for case where object is instance of class
     """
     return issubclass(type(obj), a_class) and type(obj) != a_class
