eed5d1338ce07a2ed6bad014ec0acbd5e416c5a5
starkjoy <residualcode6@gmail.com>
2023-04-11 10:56:43 -0700

    Python Inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,8 +6,8 @@ Class MyInt(int):
     """ Subclass of int implementing switch """
     def __eq__(self, value):
         """ Inverted == operator """
-        return super().__ne__(value)
+        return self.real != value
 
     def __ne__(self, value):
         """ Inverted != operator """
-        return super().__eq__(value)
+        return self.real == value
