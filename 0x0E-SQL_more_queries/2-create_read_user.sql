abac507a8e5ec44bc565196924ec6a630164c7ba
starkjoy <residualcode6@gmail.com>
2023-05-17 10:04:26 -0700

    More SQL

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,8 @@
 -- Creates a database 'hbtn_0d_2' and the user 'user_0d_2'
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+-- th
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- l
 GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- k
 FLUSH PRIVILEGES;
