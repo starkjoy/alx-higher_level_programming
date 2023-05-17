7c2760f6bd54226bff9f30df9833eda3902ad6ea
starkjoy <residualcode6@gmail.com>
2023-05-17 09:50:54 -0700

    More SQL

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Creates a SQL Server user with all privileges
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
