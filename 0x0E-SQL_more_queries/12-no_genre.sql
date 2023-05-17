a73a7db1e947c65b107a6fd49731aa26fc3b6bf6
starkjoy <residualcode6@gmail.com>
2023-05-17 11:09:30 -0700

    More SQL

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,6 @@
 -- Lists all shows contained in hbtn_0d_tvshows without a genre linked
-SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id` FROM `tv_shows`
-LEFT JOIN `tv_show_genres` ON `tv_shows.id` = `tv_show_genres`.`show_id`
-WHERE `tv_shows_genres.genre_id` IS NULL
-ORDER BY `tv_shows`.`title` ASC, `tv_show_genres`.`genre_id` ASC;
+SELECT s.`title`, g.`genre_id`
+FROM `tv_shows` AS s
+LEFT JOIN `tv_show_genres` AS g ON s.id` = g.`show_id`
+WHERE g.genre_id` IS NULL
+ORDER BY s.`title`, g.`genre_id`;
