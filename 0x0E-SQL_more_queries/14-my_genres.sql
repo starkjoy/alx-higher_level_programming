dd9f3d184c55950dca1e3b986a76cfbebc2332c9
starkjoy <residualcode6@gmail.com>
2023-05-17 11:33:19 -0700

    More SQL

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,5 +3,5 @@
 SELECT `tv_genres`.`name` FROM `tv_shows`
 INNER JOIN `tv_shows_genres` ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
 INNER JOIN `tv_genres` ON `tv_show_generes`.`genre_id` = `tv_genres`.`id`
-WHERE `tv_shows`.`title` = 'Dexter'
-ORDER BY `tv_genres`.`name` ASC;
+WHERE `tv_shows`.`title` = "Dexter"
+ORDER BY `tv_genres`.`name`;
