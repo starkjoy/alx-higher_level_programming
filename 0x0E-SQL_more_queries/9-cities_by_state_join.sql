830438f3e1793b1f456b29713b28d0d4ad3a7718
starkjoy <residualcode6@gmail.com>
2023-05-17 10:47:36 -0700

    More SQL

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,3 @@
 -- Lists all cities in database
-USE `hbtn_0d_usa`
 SELECT `cities`.`id`, `cities`.`name`, `states`.`name` FROM `cities`, `states`
 WHERE `cities`.`state_id` = `states`.`id` ORDER BY `cities`.`id` ASC;
