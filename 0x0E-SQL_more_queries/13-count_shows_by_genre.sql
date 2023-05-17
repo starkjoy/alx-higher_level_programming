5097bd1ca3fa425c1b0770c8fe84216cf0ed62d5
starkjoy <residualcode6@gmail.com>
2023-05-17 11:26:07 -0700

    More SQL

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Lists all shows contained in hbtn_0d_tvshows
 -- displaying shows linked to each genre
-SELECT `tv_shows_genres`.`genre` AS `genre`, COUNT(`tv_show_genres`.`show_id`)
+SELECT `tv_shows_genres`.`genre` AS `genre`, COUNT(*)
 AS `number_of_shows` FROM `tv_show_genres` GROUP BY `genre`
 HAVING `number_of_shows` > 0 ORDER BY `number_of_shows` DESC;
