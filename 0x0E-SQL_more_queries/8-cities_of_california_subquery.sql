8a530e1138ff40c9e7093807a35b29b1f2c1e094
starkjoy <residualcode6@gmail.com>
2023-05-17 10:41:55 -0700

    More SQL

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Lists all the cities of California in database
 SELECT * FROM `cities` WHERE `state_id` = (
-	SELECT `id` FROM `states` WHERE `name` = 'California')
+	SELECT `id` FROM `states` WHERE `name` = 'California' )
 ORDER BY `id` ASC;
