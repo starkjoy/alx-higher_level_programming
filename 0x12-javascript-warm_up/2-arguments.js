8610173b09fb94b6629eea0769031f0571c381cb
Joy <117626913+starkjoy@users.noreply.github.com>
2023-06-12 20:09:55 +0000

    Correction

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-<<<<<<< HEAD
 const argCount = process.argv.length - 2;
 if (argCount === 0){
 	console.log('No argument');
@@ -8,6 +7,3 @@ if (argCount === 0){
 } else {
 	console.log('Arguments found');
 }
-=======
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
->>>>>>> 1d0572d42b3f9027454f6be714aa5dee3ae5844d
