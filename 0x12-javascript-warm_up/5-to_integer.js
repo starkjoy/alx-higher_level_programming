6c41952ad03dc447e54275bbc63ea8534424389c
starkjoy <residualcode6@gmail.com>
2023-06-12 20:30:20 +0000

    Corrected

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const arg1 = process.argv[2];
 const num = parseInt(arg1);
 if (!isNaN(num)){
-	console.log("My number:", num);
+	console.log("My number: ", num);
 } else {
 	console.log("Not a number");
 }
