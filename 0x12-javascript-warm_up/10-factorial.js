7307f489dc176027e1f571353b4fe0db0889a268
Joy <117626913+starkjoy@users.noreply.github.com>
2023-06-12 20:59:52 +0000

    Correction

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
-const arg = parseInt(process.argv[2]);
-function factorial(n) {
-	if (isNaN(n)) {
-		return (1);
-	}
-	if (n === 0) {
-		return (1);
-	}
-	return (n * factorial(n - 1);
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
-console.log(factorial(arg));
+
+console.log(factorial(Number(process.argv[2])));
