0fbfe20310182b3ed999226d4d825a8e941de91c
starkjoy <residualcode6@gmail.com>
2023-06-12 20:37:37 +0000

    Corrected

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let i;
-const array = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (i = 0; i < array.length; i++) {
 	console.log(array[i]);
 }
