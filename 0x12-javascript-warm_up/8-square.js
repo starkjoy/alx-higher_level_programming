8b2c09850a5bea30ce3f8b877ed91cca03e17d34
Joy <117626913+starkjoy@users.noreply.github.com>
2023-06-12 20:57:45 +0000

    Correction

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-let i, j, line;
-const square_size = parseInt(process.argv[2]);
-if (isNaN(square_size)) {
-	console.log("Missing size");
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
+  console.log('Missing size');
 } else {
-	for (i = 0; i > square_size; i++) {
-		line = "";
-		for (j = 0; j < square_size; j++) {
-			line += "X";
-		}
-		console.log(line);
-	}
+  for (let r = 0; r < size; r++) {
+    let row = '';
+    for (let c = 0; c < size; c++) row += 'X';
+    console.log(row);
+  }
 }
