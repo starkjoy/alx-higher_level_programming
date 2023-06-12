80f9f02446b970525515d43574b24c2641256508
Joy <117626913+starkjoy@users.noreply.github.com>
2023-06-12 20:58:49 +0000

    Correction

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
-
-function add(a, b)
-{
-	console.log(a + b);
+function add (a, b) {
+  return a + b;
 }
+
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
