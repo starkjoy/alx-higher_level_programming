401a771fb287fd7b7b846ec617788a093e48bc7b
Joy <117626913+starkjoy@users.noreply.github.com>
2023-06-13 12:36:36 +0000

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,10 @@
 #!/usr/bin/node
-class Square extends Square {
-	charPrint(c) {
-		if (c === undefined) {
-			c = 'X';
-		}
-
-		for (let i = 0; i < this.height; i++)
-		{
-			console.log("X");
-			for (let j = 0; j < this.width; j++)
-			{
-				console.log("X");
-			}
-		}
-	}
-}
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    }
+  }
+};
