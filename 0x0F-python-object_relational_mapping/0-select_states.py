7a1150a029671300b327f44b821530d9bbd9b165
starkjoy <residualcode6@gmail.com>
2023-06-16 10:44:46 +0000

    ORMs

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -20,7 +20,7 @@ def list_states(username, password, database):
             )
 
     cursor = connection.cursor()
-    query = "SELECT * FROM states ORDER BY id ASC"
+    query = "SELECT * FROM `states` ORDER BY id ASC"
     cursor.execute(query)
     rows = cursor.fetchall()
     for row in rows:
