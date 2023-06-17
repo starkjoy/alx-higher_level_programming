5464e8cfa0a068dcab48d5338bf999ce71fee07a
starkjoy <residualcode6@gmail.com>
2023-06-17 22:04:22 +0000

    Mapping

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -12,7 +12,7 @@ Base = declarative_base()
 
 class State(Base):
     """Actual Class"""
-    __tablename__ = 'states'
+    __tablename__ = "states"
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
