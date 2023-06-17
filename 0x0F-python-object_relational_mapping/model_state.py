6356f70832e7c1a19e75f0c7620b4b94ad7183fc
starkjoy <residualcode6@gmail.com>
2023-06-17 22:05:19 +0000

    Mapping

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -16,9 +16,3 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-engine = sqlalchemy.create_engine('mysql://username:password@\
-        localhost:3306/database')
-
-Base.metadata.create_all(engine)
