3c844af930f8a8477f5a3890f96c2b481d44a4d3
starkjoy <residualcode6@gmail.com>
2023-06-17 22:01:28 +0000

    Mapping

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
+"""Creates a state class"""
+
+
 import SQLAlchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
-''' This script creates a class that contains the definition of a State and an instance '''
 
 Base = declarative_base()
 
+
 class State(Base):
+    """Actual Class"""
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
 
-engine = sqlalchemy.create_engine('mysql://username:password@localhost:3306/database')
+
+engine = sqlalchemy.create_engine('mysql://username:password@\
+        localhost:3306/database')
+
 Base.metadata.create_all(engine)
