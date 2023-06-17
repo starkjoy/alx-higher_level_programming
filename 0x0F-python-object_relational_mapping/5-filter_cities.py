d10c9b2de411b301b1b7bf8ed6dece1996dc75bb
starkjoy <residualcode6@gmail.com>
2023-06-17 21:43:49 +0000

    Mapping

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -29,7 +29,7 @@ def list_cities(username, password, database, state_name):
             )
 
     cursor = connection.cursor()
-    query = "SELECT cities.* FROM citiesJOIN states ON \
+    query = "SELECT cities.* FROM cities JOIN states ON \
             cities.state_id = states.id WHERE states.name = %s ORDER BY \
             cities.id ASC"
     cursor.execute(query, (state_name,))
