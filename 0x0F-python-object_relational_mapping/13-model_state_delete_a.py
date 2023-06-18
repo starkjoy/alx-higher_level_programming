90c686c0c6980b857a5c49d850fa31df27b6d116
starkjoy <residualcode6@gmail.com>
2023-06-18 11:34:46 +0000

    Object relations

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -5,7 +5,7 @@
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
 
 def delete_state(username, password, database):
