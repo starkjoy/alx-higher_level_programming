bacd37065381ba0294e6259ea251c5b69aeebe0c
starkjoy <residualcode6@gmail.com>
2023-06-18 11:19:00 +0000

    Object relations

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
+"""This script updates a state object in database"""
+
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
-''' This script changes the name of a state object in the database '''
-
 def delete_state(username, password, database):
-    ''' Actual implementation '''
+    """
+    Actual implementation
+
+    Args:
+        username: SQL name
+        password: SQL password
+        database: SQL database
+    """
 
     hostname = "localhost"
     port = 3306
 
-    engine = create_engine(f"mysql://{username}:{password}@{hostname}:{port}/{database}")
+    engine = create_engine(f"mysql://{username}:{password}@{hostname}:\
+            {port}/{database}")
     Session = sessionmaker(bind=engine)
     session = Session()
 
@@ -23,6 +32,7 @@ def delete_state(username, password, database):
     session.commit()
 
     session.close()
+
 
 if __name__ == "__main__":
     username = sys.argv[1]
