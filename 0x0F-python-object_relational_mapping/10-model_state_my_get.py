309095af840bb1f426b589462a03ef39c53dfa68
starkjoy <residualcode6@gmail.com>
2023-06-18 11:08:46 +0000

    Object relations

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
+"""This script prints a state object with a name from database"""
+
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
-''' This script prints a state object with a name from a database '''
-
 def list_states(username, password, database, state_name):
-    ''' Actual implementation '''
+    """
+    Actual implementation
+
+    Args:
+        username: SQL name
+        password: SQL password
+        database: SQL database
+        state_name: search string
+    """
 
     hostname = "localhost"
     port = 3306
 
-    engine = create_engine(f"mysql://{username}:{password}@{hostname}:{port}/{database}")
+    engine = create_engine(f"mysql://{username}:{password}@{hostname}:\
+            {port}/{database}")
     Session = sessionmaker(bind=engine)
     session = Session()
 
@@ -24,6 +34,7 @@ def list_states(username, password, database, state_name):
         print("Not found")
 
     session.close()
+
 
 if __name__ == "__main__":
     username = sys.argv[1]
