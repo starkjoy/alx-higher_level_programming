ac9e2cc56c8489157afc53b1b2448ed4ffdf6178
starkjoy <residualcode6@gmail.com>
2023-06-18 11:39:42 +0000

    Object relations

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
+"""This scripts lists all city objects from database"""
+
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_city import City
 
 
-''' This script lists all cities objects from a database '''
-
 def list_cities(username, password, database):
-    ''' Actual implementation '''
+    """
+    Actual implementation
+
+    Args:
+        username: SQL username
+        password: SQL password
+        database: SQL database
+    """
 
     hostname = "localhost"
     port = 3306
 
-    engine = create_engine(f"mysql://{username}:{password}@{hostname}:{port}/{database}")
+    engine = create_engine(f"mysql://{username}:{password}@{hostname}:\
+            {port}/{database}")
     Session = sessionmaker(bind=engine)
     session = Session()
 
@@ -22,6 +31,7 @@ def list_cities(username, password, database):
         state_name = city.state.name
         print(f"{state_name}: ({city.id}) {city.name}")
     session.close()
+
 
 if __name__ == "__main__":
     username = sys.argv[1]
