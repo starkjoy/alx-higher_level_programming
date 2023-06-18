5e7ae33acda2602f792121524718114e884a4057
starkjoy <residualcode6@gmail.com>
2023-06-18 11:41:41 +0000

    Object relations

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
+"""City class definition"""
+
+
 import SQLAlchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from model_state import Base
 
-''' This script creates a class that contains the definition of a city '''
-
 
 class City(Base):
+    """Actual class implementation"""
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
 
-engine = sqlalchemy.create_engine('mysql://username:password@localhost:3306/database')
-Base.metadata.create_all(engine)
+engine = sqlalchemy.create_engine('mysql://username:password@\
+        localhost:3306/database')
