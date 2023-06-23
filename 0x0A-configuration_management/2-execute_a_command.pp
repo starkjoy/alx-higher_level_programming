a40ff5f470c54782b4bbe5e9b811c180f50f4ea6
starkjoy <residualcode6@gmail.com>
2023-06-23 13:05:41 +0000

    Nothing

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# This script creates a manifest that kills a process named 'Killmenow'
-
-exec { 'kill-command':
-	command	=> 'pkill killmenow'
-	onlyif	=> 'pgrep killmenow'
-}
--- 0x0A-configuration_management/README.md
+++
@@ -1,3 +0,0 @@
-# About
----
-This Folder contains lesson files on Server Configuration Tool; Puppet.
