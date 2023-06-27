044b89da206732b2f0ed93fbd6488b73c210bca0
Joy <117626913+starkjoy@users.noreply.github.com>
2023-06-27 18:20:47 +0000

    Update 6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a peak-finding algorithm."""
+
+def find_peak(list_of_integers):
+  size = len(list_of_integers)
+  if size == 1:
+    return list_of_integers[0]
+  elif size == 2:
+    return max(list_of_integers)
+  else:
+    for i in range(1, size - 1):
+      if list_of_integers[i - 1] < list_of_integers[i] > list_of_integers[i + 1]:
+        return list_of_integers[i]
+    return None
