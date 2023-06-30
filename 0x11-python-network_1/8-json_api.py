48360417a7ef955281900b8102a5e6a2b3ae6fc8
starkjoy <residualcode6@gmail.com>
2023-06-30 18:38:28 +0000

    Python Files on Networking

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Sends a POST request to stated url with a letter as a paramater """
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    r = request.post("http://0.0.0.0:5000/searh_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
